8cd01edc5984adbcfbc66d074b1c04fd5dcfc5b9
taiyooooooooooo <30978827+taiyooooooooooo@users.noreply.github.com>
2021-07-17 01:45:07 +0900

    update Rowコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { requests } from './request';
+import { Row } from './Row';
 
 function App() {
   return (
     <div className="App">
+      <Row
+        title="NETFIX ORIGINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      ></Row>
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="Documentaries Movies" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
