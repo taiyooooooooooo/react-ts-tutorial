31076dc5b0ed3157aab6973d48e9bb45489f35c7
taiyooooooooooo <30978827+taiyooooooooooo@users.noreply.github.com>
2021-07-17 01:49:28 +0900

    update 具体的なコンポーネントを追加

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,5 +1,6 @@
 import axios from "./axios";
 import { useEffect, useState } from "react";
+import { EYE_CATCH_BASE_URL } from "./request";
 
 type Props = {
     title: string;
@@ -26,6 +27,19 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
         fetchData();
     }, [fetchUrl]);
     return (
-        <div className="Row" />
+        < div >
+            <h2>{title}</h2>
+            {
+                movies.map((movie) => {
+                    return (
+                        < img key={movie.id}
+                            className={`Row-poster ${isLargeRow && "Row-poster-large"}`}
+                            src={EYE_CATCH_BASE_URL + movie.backdrop_path}
+                        ></img>
+
+                    )
+                })
+            }
+        </div >
     )
 }
